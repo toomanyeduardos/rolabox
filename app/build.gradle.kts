157756711ab66a57a9df2a47e2c14269c2897ab3
Eduardo Flores <toomanyeduardos@gmail.com>
2026-09-23 20:15:56 -0600

    gradle fun

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,13 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
+    id("rolabox.android.application")
+    id("rolabox.android.compose")
 }
 
 android {
     namespace = "com.eduardoflores.rolabox"
-    compileSdk {
-        version = release(37)
-    }
 
     defaultConfig {
         applicationId = "com.eduardoflores.rolabox"
-        minSdk = 29
-        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
@@ -26,29 +21,14 @@ android {
             }
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-    debugImplementation(libs.androidx.compose.ui.tooling)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
